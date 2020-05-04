797908ca86c7ec6ea23cd38180541e1d091b49db
Marek Pridal <marek@marekpridal.eu>
2020-05-04 16:39:44 +0200

    Export xcframework into artifacts folder

--- create-xcframework.sh
+++ create-xcframework.sh
@@ -57,11 +57,11 @@ SKIP_INSTALL=NO \
 BUILD_LIBRARY_FOR_DISTRIBUTION=YES
 
 xcodebuild -create-xcframework \
--framework "Archives/LogKit-iOS.xcarchive/Products/Library/Frameworks/LogKit.framework" \
--framework "Archives/LogKit-iOS-Simulator.xcarchive/Products/Library/Frameworks/LogKit.framework" \
--framework "Archives/LogKit-watchOS.xcarchive/Products/Library/Frameworks/LogKit.framework" \
--framework "Archives/LogKit-watchOS-Simulator.xcarchive/Products/Library/Frameworks/LogKit.framework" \
--framework "Archives/LogKit-tvOS.xcarchive/Products/Library/Frameworks/LogKit.framework" \
--framework "Archives/LogKit-tvOS-Simulator.xcarchive/Products/Library/Frameworks/LogKit.framework" \
--framework "Archives/LogKit-macOS.xcarchive/Products/Library/Frameworks/LogKit.framework" \
--output LogKit.xcframework
+-framework "archives/LogKit-iOS.xcarchive/Products/Library/Frameworks/LogKit.framework" \
+-framework "archives/LogKit-iOS-Simulator.xcarchive/Products/Library/Frameworks/LogKit.framework" \
+-framework "archives/LogKit-watchOS.xcarchive/Products/Library/Frameworks/LogKit.framework" \
+-framework "archives/LogKit-watchOS-Simulator.xcarchive/Products/Library/Frameworks/LogKit.framework" \
+-framework "archives/LogKit-tvOS.xcarchive/Products/Library/Frameworks/LogKit.framework" \
+-framework "archives/LogKit-tvOS-Simulator.xcarchive/Products/Library/Frameworks/LogKit.framework" \
+-framework "archives/LogKit-macOS.xcarchive/Products/Library/Frameworks/LogKit.framework" \
+-output artifacts/LogKit.xcframework
