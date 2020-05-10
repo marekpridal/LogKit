8c46eafb19fe75abcb780c778462157647ae8409
Marek Pridal <marek@marekpridal.eu>
2020-05-10 15:48:40 +0200

    Fix swiftlint warnings

--- Tests/LogKitTests/LogKitTests.swift
+++ Tests/LogKitTests/LogKitTests.swift
@@ -17,66 +17,72 @@ final class LogKitTests: XCTestCase {
         ]
         return log
     }
-    
+
+    private struct Foo: Encodable {
+        let bar: String
+    }
+
     func testLogDeinit() {
         log.deinit(of: self)
     }
-    
+
     func testLogFunctionIn() {
         log.function(#function, in: #file)
         log.function(#function, text: "text")
     }
-    
+
     func testLogDefault() {
         log.default("default")
     }
-    
+
     func testLogRequestCalled() {
         log.requestCalled(function: #function)
     }
-    
+
     func testLogExpiration() {
         log.expiration(date: .init())
     }
-    
+
     func testLogRequest() {
-        log.request(URLRequest(url: URL(string: "https://github.com/marekpridal/LogKit")!, cachePolicy: .useProtocolCachePolicy, timeoutInterval: 30))
+        log.request(URLRequest(url: URL(string: "https://github.com/marekpridal/LogKit")!,
+                               cachePolicy: .useProtocolCachePolicy,
+                               timeoutInterval: 30))
     }
-    
+
     func testLogResponse() {
-        struct Foo: Encodable {
-            let bar: String
-        }
-        
         let encoded = try? JSONEncoder().encode(Foo(bar: "Hello world"))
-        
-        log.response(URLResponse(url: URL(string: "https://github.com/marekpridal/LogKit")!, mimeType: nil, expectedContentLength: 30, textEncodingName: "application/json"), data: encoded)
+
+        log.response(URLResponse(url: URL(string: "https://github.com/marekpridal/LogKit")!,
+                                 mimeType: nil,
+                                 expectedContentLength: 30,
+                                 textEncodingName: "application/json"),
+                     data: encoded)
     }
-    
+
     func testLogInAppPurchase() {
         log.inAppPurchase("Purchasing...")
     }
-    
+
     func testLogError() {
         log.error(NSError(domain: "logkit.tests", code: 0, userInfo: nil))
     }
-    
+
     func testLogSKRequest() {
         log.products(request: .init(productIdentifiers: ["logkit_product_identifier"]))
     }
-    
+
     func testLogSKProductsResponse() {
         log.products(response: .init())
     }
-    
+
     func testLogSKProductsRequest() {
         log.products(request: SKRequest())
     }
-    
+
     func testLogPaymentQueue() {
         log.paymentQueue(.default())
     }
-    
+
     func testLogPayment() {
         log.payment(transactions: [.init()])
     }
